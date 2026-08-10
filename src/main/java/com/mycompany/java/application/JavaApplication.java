3105c97acda4fff98ca9bd1b4590b655e67cf867
Evandro Wagencknecht <evandrobch@hotmail.com>
2026-08-10 17:30:53 -0300

    Projeto CloudSim A3

---
+++ src/main/java/com/mycompany/java/application/JavaApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.application;
+
+/**
+ *
+ * @author gorfa
+ */
+public class JavaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
